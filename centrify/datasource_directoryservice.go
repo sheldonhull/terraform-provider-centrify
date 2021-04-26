d6bd1739d84d30202df1080c0a87d0a051df2f89
Fab Viguier <badrogh@gmail.com>
2021-04-26 13:51:20 +0100

    Update datasource_directoryservice.go

--- centrify/datasource_directoryservice.go
+++ centrify/datasource_directoryservice.go
@@ -6,10 +6,10 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
-	"github.com/marcozj/golang-sdk/enum/directoryservice"
-	logger "github.com/marcozj/golang-sdk/logging"
-	vault "github.com/marcozj/golang-sdk/platform"
-	"github.com/marcozj/golang-sdk/restapi"
+	"github.com/centrify/terraform-provider-centrify/golang-sdk/enum/directoryservice"
+	logger "github.com/centrify/terraform-provider-centrify/golang-sdk/logging"
+	vault "github.com/centrify/terraform-provider-centrify/golang-sdk/platform"
+	"github.com/centrify/terraform-provider-centrify/golang-sdk/restapi"
 )
 
 func dataSourceDirectoryService() *schema.Resource {
